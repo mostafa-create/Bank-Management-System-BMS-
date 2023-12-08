be7d7c0102a1c35a619f8b5fab5f43e82aa468ba
Mostafa Yassin <112202482+mostafa-create@users.noreply.github.com>
2023-12-08 20:09:09 +0200

    Update database.h

--- database.h
+++ database.h
@@ -8,39 +8,37 @@
 #include <stdlib.h>
 #define ll long long    
 using namespace std;
-struct user {
-	string Name, Password;
-	ll Accnum, Balance;
-};
-struct transfer {
-
-	ll Banktype, Fromacc, Toacc, Amount;
-};
-struct phone_wallet {
-	string number, password;
-	ll Balance;
-};
-vector < transfer > Tran;
-vector < phone_wallet > Wallets;
-vector < user > bankmasr;
-vector < user > nationalbank;
-vector < user > agribank;
-vector < user > arabafricanbank;
-vector < user > cibbank;
-vector < user > faisalbank;
-vector < string > Banks = { "BM.txt","NB.txt","CIB.txt","AB.txt","AAB.txt","FIB.txt" };
-user p;
-transfer m;
 class database {
 public:
+	struct user {
+		string Name, Password;
+		ll Accnum, Balance;
+	};
+	struct transfer {
+
+		ll Banktype, Fromacc, Toacc, Amount;
+	};
+	struct phone_wallet {
+		string number, password;
+		ll Balance;
+	};
+	vector < transfer > Tran;
+	vector < phone_wallet > Wallets;
+	vector < user > bankmasr;
+	vector < user > nationalbank;
+	vector < user > agribank;
+	vector < user > arabafricanbank;
+	vector < user > cibbank;
+	vector < user > faisalbank;
+	vector < string > Banks = { "BM.txt","NB.txt","CIB.txt","AB.txt","AAB.txt","FIB.txt" };
 	database();
+	void upload(ll, ll, ll, ll);
+	void upload(string, string, ll);
 	void upload_users_bank(ll, vector < user >(&));
 	void download_users_bank(ll, vector < user >(&));
 	void download_all_transfers();
 	void upload_all_transfers();
-	void upload_transfer(ll, ll, ll, ll);
-	void upload_wallet(string, string, ll);
 	void upload_all_wallets();
 	void download_all_wallets();
-	bool num_validation(string);
+	virtual bool num_validation(string) = 0;
 };
